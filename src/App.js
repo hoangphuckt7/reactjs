0672f7a1b48a5b3a90bb56fe43ed6a5374975725
Hoang Phuc <hoangphuckt7@gmail.com>
2021-08-21 00:25:12 +0700

    test

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hoàng Phúc</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hoàng Phúc đẹp trai</p>
       </header>
     </div>
   );
